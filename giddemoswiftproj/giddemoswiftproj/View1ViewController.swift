12e88d49a2699c6df8147a7d05b39c456cf91bbf
Shangin Vyacheslav <slava-poison@yandex.ru>
2021-02-20 21:34:52 +0400

    new file and button

---
+++ giddemoswiftproj/giddemoswiftproj/View1ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  View1ViewController.swift
+//  giddemoswiftproj
+//
+//  Created by Вячеслав Шангин on 20.02.2021.
+//
+
+import UIKit
+
+class View1ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
